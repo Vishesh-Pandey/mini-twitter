2ce33c90bfddbd0aaa9915436648dddb5380aba4
Vishesh Pandey <74998585+Vishesh-Pandey@users.noreply.github.com>
2023-06-16 04:31:43 +0530

    1 - add: Homepage UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col-2 border-end">
+          <i class="bi bi-twitter h1 text-primary"></i>
+        </div>
+        <div className="col-6">
+          <div className="row">
+            <div className="col-12 py-2">
+              <h4>Home</h4>
+            </div>
+            <div className="col-12">
+              <div className="row border border-2 border-start-0 border-end-0 py-2">
+                <div className="col-2">
+                  <i class="bi bi-person-circle h1"></i>
+                </div>
+                <div className="col-8 d-flex align-items-center">
+                  <input
+                    type="text"
+                    className="form-control border-0"
+                    placeholder="Whats happening?"
+                  />
+                </div>
+                <div className="col-2 d-flex align-items-center">
+                  <button className="btn btn-primary w-100">Tweet </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-4 border-start">
+          <div className="row">
+            <div className="col-12 py-2">
+              <input
+                type="text"
+                className="form-control bg-secondary bg-opacity-25"
+                placeholder="Search Twitter"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
